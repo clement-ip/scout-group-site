7ab610a308fec1a33c36eedc5e46da3cfc316631
Clement Ip <clement.ip96@gmail.com>
2021-02-26 00:22:11 -0800

    bootstrap works without vendor file

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import About from './components/About.js';
 import FAQ from './components/FAQ.js';
 import Contact from './components/Contact.js';
 
+// import 'bootstrap/dist/css/bootstrap.css'; 
+// import 'bootstrap/dist/js/bootstrap.js';
+
 function App() {
   return (
     <div className="App">
